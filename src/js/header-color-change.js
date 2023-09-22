da9a3cfa3fbe4fdd5d30f57e46d669db19e889f9
manforowicz <manforowicz@gmail.com>
2023-09-22 08:59:08 -0700

    Reogranized files, and made header white for no-js users

--- src/js/header-color-change.js
+++ src/js/header-color-change.js
@@ -1,12 +1,10 @@
-window.onload = function () {
+var header = document.getElementById('navbar');
+header.classList.remove("navbar-scrolled");
 
-    var header = document.getElementById('navbar');
-
-    document.addEventListener('scroll', function (e) {
-        if (window.scrollY > 0) {
-            header.classList.add("navbar-scrolled");
-        } else {
-            header.classList.remove("navbar-scrolled");
-        }
-    });
-}
+document.addEventListener('scroll', function (e) {
+    if (window.scrollY > 0) {
+        header.classList.add("navbar-scrolled");
+    } else {
+        header.classList.remove("navbar-scrolled");
+    }
+});
