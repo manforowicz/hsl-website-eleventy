80a59c1540cef97377ff665236a43302ea0956cd
Marcin Anforowicz <manforowicz@gmail.com>
2024-04-02 21:05:36 -0700

    Stylistic changes

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,7 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/img/");
-    eleventyConfig.addWatchTarget("src/img/");
-
     eleventyConfig.addPassthroughCopy("src/css/");
-    eleventyConfig.addWatchTarget("src/css/");
-
-
     eleventyConfig.addPassthroughCopy("src/js/");
-    eleventyConfig.addWatchTarget("src/js/");
 
     return {
       dir: {
